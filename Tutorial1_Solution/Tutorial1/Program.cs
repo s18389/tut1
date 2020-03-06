e4c361102cdeae97441ffb56b609aad2d39a42a4
s18389 <s18389@pjwstk.edu.pl>
2020-03-06 09:58:40 +0100

    Modified the project

--- Tutorial1_Solution/Tutorial1/Program.cs
+++ Tutorial1_Solution/Tutorial1/Program.cs
@@ -9,26 +9,45 @@ namespace Tutorial1
     {
         public static async Task Main(string[] args)
         {
-            var websiteUrl = args[0];
+             var websiteUrl = args.Length > 0 ? args[0] : throw new ArgumentNullException();
+            ///string websiteUrl = null;
+           
+            // var x = websiteUrl ?? throw new ArgumentException("URL not null!");
+            
             var httpClient = new HttpClient();
-            var response = await httpClient.GetAsync(websiteUrl); //working as a thread
-            if (response.IsSuccessStatusCode)
+            try
             {
-                var htmlContent = await response.Content.ReadAsStringAsync();
-                //ctrl + shift + space 
-                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
-
-                var emailAddresses = regex.Matches(htmlContent);
-
-                //tab to do the tample for method
-                foreach (var emailAddress in emailAddresses)
+                var response = await httpClient.GetAsync(websiteUrl); //working as a thread
+                httpClient.Dispose();
+          
+                if (response.IsSuccessStatusCode)
                 {
-                    Console.WriteLine(emailAddress.ToString());
+                    var htmlContent = await response.Content.ReadAsStringAsync();
+                    //ctrl + shift + space 
+                    var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+                    var emailAddresses = regex.Matches(htmlContent);
+
+                    //tab to do the tample for method
+                    if(emailAddresses.Count > 0)
+                    {
+                        foreach (var emailAddress in emailAddresses)
+                        {
+                            Console.WriteLine(emailAddress.ToString());
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No email addreeses!");
+                    }
                 }
-
             }
-            Console.ReadKey();
+            catch (Exception)
+            {
+                Console.WriteLine("ERROR when downloading the page");
+            }
 
+            Console.ReadKey();
         }
     }
 }
