f1fdb8267dc200914343cf599163a49d08132abe
Jakub Michalski <s18389@pjwstk.edu.pl>
2020-03-06 09:29:02 +0100

    inutial commit

---
+++ Tutorial1_Solution/Tutorial1/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tutorial1
+{
+    class Class1
+    {
+        public int Attribute1 { get; set; }
+
+        // java: string var1 = "dsds";
+    }
+}
---
+++ Tutorial1_Solution/Tutorial1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Tutorial1
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var websiteUrl = args[0];
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(websiteUrl); //working as a thread
+            if (response.IsSuccessStatusCode)
+            {
+                var htmlContent = await response.Content.ReadAsStringAsync();
+                //ctrl + shift + space 
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+                var emailAddresses = regex.Matches(htmlContent);
+
+                //tab to do the tample for method
+                foreach (var emailAddress in emailAddresses)
+                {
+                    Console.WriteLine(emailAddress.ToString());
+                }
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
